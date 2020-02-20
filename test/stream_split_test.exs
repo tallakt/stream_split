a6c1c4bb22724dbd19f1e5a13637cddc4655b4c3
Mateusz Front <mateusz.front@swmansion.com>
2020-02-20 15:39:53 +0100

    fix memleak

--- test/stream_split_test.exs
+++ test/stream_split_test.exs
@@ -8,4 +8,27 @@ defmodule StreamSplitTest do
     {elements, []} = StreamSplit.take_and_drop(stream, 1)
     assert elements == []
   end
+
+  test "multiple pops" do
+    {1, stream} = Stream.cycle([1, 2, 3]) |> StreamSplit.pop()
+    {2, stream} = stream |> StreamSplit.pop()
+    {3, stream} = stream |> StreamSplit.pop()
+    {1, _stream} = stream |> StreamSplit.pop()
+  end
+
+  test "does not leak" do
+    {_, stream} = Stream.cycle([1, 2, 3]) |> StreamSplit.pop()
+
+    stream_after_some_pops =
+      1..1000
+      |> Enum.reduce(stream, fn _, stream_acc ->
+        {_, stream_acc} = StreamSplit.pop(stream_acc)
+        stream_acc
+      end)
+
+    init_size = byte_size(:erlang.term_to_binary(stream))
+    final_size = byte_size(:erlang.term_to_binary(stream_after_some_pops))
+
+    assert_in_delta final_size / init_size, 1, 0.1
+  end
 end
